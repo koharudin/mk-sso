fd58347dfedccd27287dcbc6c4eb3a6ce912921b
Koharudin <koharudin.mail07@gmail.com>
2024-05-30 12:49:15 +0700

    add riwayat organisasi

--- src/Pelayanan/WizardBuatUsulan/StepWizardPanelLayanan.jsx
+++ src/Pelayanan/WizardBuatUsulan/StepWizardPanelLayanan.jsx
@@ -1,14 +1,13 @@
 import { Button } from 'react-bootstrap';
 import ButtonWizard from './ButtonWizard';import PanelRiwayatJabatan from '../panels/PanelRiwayatJabatan';
 
-import PanelRiwayatNikah from '../panels/PanelRiwayatNikah';
-import PanelRiwayatSaudara from '../panels/PanelRiwayatSaudara';
+import PanelRiwayatOrganisasi from '../panels/PanelRiwayatOrganisasi';
 
 const StepWizardPanelLayanan = (props) => {
   return (
     <>
       Panel Layanan <hr></hr>
-      <PanelRiwayatSaudara />
+      <PanelRiwayatOrganisasi />
       <div>
         <hr></hr>
         <ButtonWizard {...props}></ButtonWizard>
---
+++ src/Pelayanan/panels/PanelRiwayatOrganisasi.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from 'react';
+import { Badge, Button, Card, Form, Table } from 'react-bootstrap';
+
+import Moment from 'react-moment';
+import GridUsulanRiwayat from '../components/GridUsulanRiwayat';
+import { NumericFormat, PatternFormat, numericFormatter } from 'react-number-format';
+
+import FormRiwayatPengalamanKerja from '../forms/FormRiwayatPengalamanKerja';
+import FormRiwayatOrganisasi from '../forms/FormRiwayatOrganisasi';
+
+const FormInput = (props) => {
+  const onBatal = () => {
+    props.setActivePanel('grid');
+  };
+
+  return (
+    <Card>
+      <Card.Header>
+        <Card.Title as="h5">Form Riwayat Organisasi</Card.Title>
+      </Card.Header>
+      <Card.Body>
+        <FormRiwayatOrganisasi {...props} />
+      </Card.Body>
+    </Card>
+  );
+};
+const DaftarRiwayat = (props) => {
+  const onCreateNew = () => {
+    props.setActivePanel('form');
+  };
+
+  return (
+    <GridUsulanRiwayat
+      onCreateNew={onCreateNew}
+      title={'Daftar Riwayat Organisasi'}
+      grid_url={'/riwayat-organisasi'}
+      cols={[
+        {
+          label: 'NAMA',
+          field: 'nama'
+        },
+        {
+          label: 'JABATAN',
+          field: 'jabatan'
+        },
+        {
+          label: 'AWAL',
+          field: 'awal',
+          formatter: function (value, row, index) {
+            return <Moment date={value} format="DD/MMM/YYYY" />;
+          }
+        },
+        {
+          label: 'AKHIR',
+          field: 'akhir',
+          formatter: function (value, row, index) {
+            return <Moment date={value} format="DD/MMM/YYYY" />;
+          }
+        },
+        {
+          label: 'PIMPINAN',
+          field: 'pimpinan'
+        },
+        {
+          label: 'TEMPAT',
+          field: 'tempat'
+        }
+      ]}
+    />
+  );
+};
+export default (props) => {
+  const [activePanel, setActivePanel] = useState('grid');
+  return (
+    <>
+      {activePanel == 'grid' && <DaftarRiwayat setActivePanel={setActivePanel}></DaftarRiwayat>}
+      {activePanel == 'form' && <FormInput setActivePanel={setActivePanel}></FormInput>}
+    </>
+  );
+};
