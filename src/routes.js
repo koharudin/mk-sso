7319ad6a99cb77e6a1aeaaa65739ded9b32782f7
Koharudin <koharudin.mail07@gmail.com>
2024-06-22 18:36:45 +0700

    clear prod

--- src/routes.js
+++ src/routes.js
@@ -54,125 +54,20 @@ export const renderAuthenticatedRoutes = (routes = []) => (
 export const authenticatedRoutes = [
   {
     exact: 'true',
-    path: '/usulan-ku/daftar-usulan',
-    element: lazy(() => import('./Pelayanan/GridUsulanLayanan'))
-  },
-  {
-    exact: 'true',
-    path: '/verifikasi-usulan',
-    element: lazy(() => import('./Pelayanan/VerifikasiUsulanLayanan'))
-  },
-  {
-    exact: 'true',
-    path: '/verifikasi-usulan/:uuid_usulan/detail',
-    element: lazy(() => import('./Pelayanan/DetailVerifikasiUsulan'))
-  },
-  {
-    exact: 'true',
-    path: '/usulan-ku/buat-baru',
-    element: lazy(() => import('./Pelayanan/WizardUsulan'))
-  },
-  {
-    exact: 'true',
-    path: '/usulan-ku/daftar-usulan/:kategori_layanan/baru',
-    element: lazy(() => import('./Pelayanan/BuatUsulan'))
-  },
-  {
-    exact: 'true',
-    path: '/daftar-usulan/:uuid_usulan/detail',
-    element: lazy(() => import('./Pelayanan/DetailUsulan'))
-  },
-  {
-    exact: 'true',
-    path: '/usulan-ku/daftar-usulan/:id_usulan/edit',
-    element: lazy(() => import('./Pelayanan/EditUsulan'))
-  },
-  {
-    exact: 'true',
     path: '/dashboard',
-    element: lazy(() => import('./Pelayanan/Dashboard'))
-  },
-  {
-    exact: 'true',
-    path: '/basic/button',
-    element: lazy(() => import('./views/ui-elements/basic/BasicButton'))
-  },
-  {
-    exact: 'true',
-    path: '/basic/badges',
-    element: lazy(() => import('./views/ui-elements/basic/BasicBadges'))
-  },
-  {
-    exact: 'true',
-    path: '/basic/breadcrumb',
-    element: lazy(() => import('./views/ui-elements/basic/BasicBreadcrumb'))
-  },
-  {
-    exact: 'true',
-    path: '/basic/pagination',
-    element: lazy(() => import('./views/ui-elements/basic/BasicPagination'))
-  },
-  {
-    exact: 'true',
-    path: '/basic/collapse',
-    element: lazy(() => import('./views/ui-elements/basic/BasicCollapse'))
-  },
-  {
-    exact: 'true',
-    path: '/basic/tabs-pills',
-    element: lazy(() => import('./views/ui-elements/basic/BasicTabsPills'))
-  },
-  {
-    exact: 'true',
-    path: '/basic/typography',
-    element: lazy(() => import('./views/ui-elements/basic/BasicTypography'))
-  },
-  {
-    exact: 'true',
-    path: '/forms/form-basic',
-    element: lazy(() => import('./views/forms/FormsElements'))
-  },
-  {
-    exact: 'true',
-    path: '/tables/bootstrap',
-    element: lazy(() => import('./views/tables/BootstrapTable'))
-  },
-  {
-    exact: 'true',
-    path: '/charts/nvd3',
-    element: lazy(() => import('./views/charts/nvd3-chart'))
-  },
-  {
-    exact: 'true',
-    path: '/maps/google-map',
-    element: lazy(() => import('./views/maps/GoogleMaps'))
-  },
-  {
-    exact: 'true',
-    path: '/sample-page',
-    element: lazy(() => import('./views/extra/SamplePage'))
+    element: lazy(() => import('./App/Dashboard'))
   }
 ];
 const routes = [
   {
     exact: 'true',
     path: '/login',
-    element: lazy(() => import('./Pelayanan/Login'))
+    element: lazy(() => import('./App/Login'))
   },
   {
     exact: 'true',
     path: '/authorisasi',
-    element: lazy(() => import('./Pelayanan/Authorisasi'))
-  },
-  {
-    exact: 'true',
-    path: '/test',
-    element: lazy(() => import('./Pelayanan/test/Test'))
-  },
-  {
-    exact: 'true',
-    path: '/',
-    element: (lazy(() => import('./Pelayanan/Homepage')))
+    element: lazy(() => import('./App/Authorisasi'))
   }
 ];
 
